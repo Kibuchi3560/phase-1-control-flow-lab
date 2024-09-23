9b4a5119fad7b774dff596d56d649d476fda2fb3
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-09-23 22:18:21 +0300

    Works on Control flaw lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceCovered){
   // Write your code here!
+  
+  if(distanceCovered <= 400){
+    return"This one is on me!";
+ }
+ else if(distanceCovered <= 2000){
+  return"That will be twenty bucks."; 
+ }
+ else if(distanceCovered <= 2500){
+  return"I will gladly take your thirty bucks.";
+ }
+ else {
+  return"No can do.";
+ }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city === "NYC" ? "Ok, sounds good." : "No go.";
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+    return"Thank you so much.";
+
+  case "not as generous":
+    return "Thank you.";
+
+    default : 
+    return"Bye.";
+  }
+
 }
